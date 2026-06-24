72fd66f6d811b59bffba7fb8893c1384eab7ec99
dvytvs <zapretsystem445@proton.me>
2026-06-24 19:49:07 +0500

    Fix text tool resize handles and add RPM build

---
+++ electron/preload.cjs
@@ -0,0 +1,16 @@
+const { contextBridge, ipcRenderer } = require('electron');
+
+contextBridge.exposeInMainWorld('electronAPI', {
+  getInitialFile: () => ipcRenderer.invoke('get-initial-file'),
+  newWindow: () => ipcRenderer.invoke('new-window'),
+  openFile: () => ipcRenderer.invoke('open-file'),
+  saveFile: (data) => ipcRenderer.invoke('save-file', data),
+  getPrinters: () => ipcRenderer.invoke('get-printers'),
+  print: () => ipcRenderer.invoke('print'),
+  setWallpaper: (dataUrl) => ipcRenderer.invoke('set-wallpaper', dataUrl),
+  share: (dataUrl) => ipcRenderer.invoke('share', dataUrl),
+  exit: (force) => ipcRenderer.invoke('exit', force),
+  windowControl: (action) => ipcRenderer.invoke('window-control', action),
+  onCloseRequested: (callback) => ipcRenderer.on('request-close', callback),
+  offCloseRequested: () => ipcRenderer.removeAllListeners('request-close')
+});
